27a9c37bb9e81aa66aeb1ede1df6386bc1dc0946
C K <ctkoo@ucsc.edu>
2022-07-18 01:54:48 -0700

    brainstorm

---
+++ index.js
@@ -0,0 +1,19 @@
+//Prompts
+
+//Information on application repository
+
+//Title - Displayed as title of README
+
+//Table of Contents
+
+//Installation
+
+//Usage
+
+//License - Added near top of README, which license covered under
+
+//Contributing
+
+//Tests
+
+//Questions - Adds Github username with link to profile, and email
