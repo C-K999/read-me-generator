db489d1be93671615f60e250584e33bbfb6e4a3a
C K <ctkoo@ucsc.edu>
2022-07-18 18:58:34 -0700

    not sure if this is how im supposed to do this

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
-const inquirer = require('inquirer');
 const fs = require('fs');
+const inquirer = require('inquirer');
 const axios = require("axios");
+const { table } = require('console');
 
-//Prompts
+inquirer
+    .prompt([
+        {
+            type:'input',
+            message:"What's the name of your project?",
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Please provide your project a description.',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'Please describe the installation process to serve as an instruction.',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'Now, please provide usage information for your project.',
+            name: 'usage',
+        },
+        
+    ])
+    .then((response) => {
+        const filename  = response.title.split(' ').join('-')+".md";
+
+        const readTitle = '# '+response.title+'\n';
+        const readDesc = '\n'+response.description+'\n';
+        const tableCont = '\n ## Table of Contents\n'+
+                        '\n - [Installation](#installation)'+
+                        '\n - [Usage](#usage)'+
+                        '\n - [License](#license)'+
+                        '\n - [Contributing](#contribute)'+
+                        '\n - [Testing](#testing)'+
+                        '\n - [Questions](#questions)\n'; 
+
+        const readInstall = '\n ## Installation\n'+ '\n'+response.installation+'\n';
+        const readUsage = '\n ## Usage\n'+ '\n'+response.usage+'\n';
+        
+        const output = readTitle+readDesc+tableCont+readInstall;
+
+        console.log(output);
+    })
+
+//Prompts:
 
 //Information on application repository
 
 //Title - Displayed as title of README
 
-//Table of Contents
-
-//Installation
-
-//Usage
-
 //License - Added near top of README, which license covered under
-
 //Contributing
-
 //Tests
-
 //Questions - Adds Github username with link to profile, and email
 
 /* References
